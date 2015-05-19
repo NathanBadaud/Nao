24108d8deedd32b3ebc95c0c6b9a9d82baf437d3
Nathan <nathan@badaud.fr>
2015-05-19 10:43:19 +0200

    Création des classes BNW

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,8 @@
  * Created by Walrand on 19/05/2015.
  */
 public class Main {
+/*
+	Il faut que le main lance nos différentes classes.
+	Chacunes de nos classes demanderont à NAO des fonctionnalités 
+ */
 }
---
+++ src/Nathan.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Nathan on 19/05/2015.
+ */
+public class Nathan {
+}
